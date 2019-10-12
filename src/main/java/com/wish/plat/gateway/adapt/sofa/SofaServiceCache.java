a025daee47e935c731b9e5b43f2877a652263be9
Y01610 <quanxin@yondervision.com.cn>
2019-10-12 16:10:17 +0800

    去掉隔天清理转发的缓存

--- src/main/java/com/wish/plat/gateway/adapt/sofa/SofaServiceCache.java
+++ src/main/java/com/wish/plat/gateway/adapt/sofa/SofaServiceCache.java
@@ -9,13 +9,17 @@ import com.google.common.cache.CacheBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * @author: QUAN
  * @date: Created in 2019/8/2 14:07
  * @description: 增加代理的缓存类（参考袁绍鹏的代码）
  * 1发现服务调用三次后会失败，2提高访问效率
+ * remark :20191012
+ * 此缓存类可以用一个静态map代替
+ * 问题：目前配置的是一天后会清理缓存，这样周一调用，晚上清楚。周二、周三，都没问题
+ * 周四调用，却出现出现三次的异常
+ * 原因：不能设置为每天清理一次或者替代掉该方案
+ * 此次先注释掉 ： .expireAfterWrite(1, TimeUnit.DAYS)
  * @modified By:
  */
 public class SofaServiceCache {
@@ -26,7 +30,7 @@ public class SofaServiceCache {
     private static Cache<String, GenericService> sofaServiceCache =
             CacheBuilder.newBuilder()
                     .concurrencyLevel(8)
-                    .expireAfterWrite(1, TimeUnit.DAYS)
+                    // .expireAfterWrite(1, TimeUnit.DAYS)
                     .initialCapacity(50)
                     .maximumSize(300)
                     .recordStats()
